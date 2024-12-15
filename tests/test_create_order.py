5708e7a0da77705b8b5780b99579d4009164ce47
Sergey Korneev <Sempela@yandex.ru>
2024-12-15 13:31:29 +0300

    убрал принты изз крейт ордер

--- tests/test_create_order.py
+++ tests/test_create_order.py
@@ -11,25 +11,21 @@ class TestCreateOrder:
     def test_create_order_auth(self, create_and_delete_user):
         access_token = create_and_delete_user['access_token']
         response = order_methods.create_order(VALID_INGREDIENTS_PAYLOAD, access_token)
-        print(response)
         assert response.status_code == 200 and response.json()['success'] is True
 
     @allure.title('создание заказа с без авторизации')
     def test_create_order_without_auth(self, create_and_delete_user):
         response = order_methods.create_order(VALID_INGREDIENTS_PAYLOAD, '')
-        print(response)
         assert response.status_code == 200 and response.json()['success'] is True
 
     @allure.title('создание заказа с без ингредиентов')
     def test_create_without_ing(self, create_and_delete_user):
         access_token = create_and_delete_user['access_token']
         response = order_methods.create_order(EMPTY_INGREDIENTS_PAYLOAD, access_token)
-        print(response)
         assert response.status_code == 400 and response.json()['success'] is False
 
     @allure.title('создание заказа с неверным хешем ингредиента')
     def test_invalid_hash_ing(self, create_and_delete_user):
         access_token = create_and_delete_user['access_token']
         response = order_methods.create_order(INVALID_INGREDIENTS_PAYLOAD, access_token)
-        print(response)
         assert response.status_code == 400 and response.json()['success'] is False
